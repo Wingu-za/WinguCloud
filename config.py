09417662b1f0f716da6e70025798604a63a09df2
Allan <allanice001@users.noreply.github.com>
2018-02-21 09:55:47 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+STACKNAME = "demo-stack"
+AUTH_URL = 'https://identity.wingu.co.za:5000/v2.0'                                  
+USERNAME = 'your-winu@email.address'
+PASSWORD = 'YourSuperSecretPassword'
+PROJECT_ID = 'c97ad1c671ec466c9c47437fa1496f54'
+HEAT_URL = 'https://orchestration.wingu.co.za:8004/v1/c97ad1c671ec466c9c47437fa1496f54'
